b53ec7a24f3e7b484c0e314835d1c0b838292fa7
Merlin <hugemerlin91@gmail.com>
2026-01-29 09:39:55 +0100

    Lexer and Parser, fali oporavak od greske v5

--- test/rs/ac/bg/etf/pp1/Compiler.java
+++ test/rs/ac/bg/etf/pp1/Compiler.java
@@ -21,7 +21,7 @@ public class Compiler {
         Logger log = Logger.getLogger(Compiler.class);
         Reader br = null;
         try {
-            File sourceCode = new File("test/program.mj");
+            File sourceCode = new File("test/test302.mj");
             log.info("Compiling source file: " + sourceCode.getAbsolutePath());
             br = new BufferedReader(new FileReader(sourceCode));
             Yylex lexer = new Yylex(br);
